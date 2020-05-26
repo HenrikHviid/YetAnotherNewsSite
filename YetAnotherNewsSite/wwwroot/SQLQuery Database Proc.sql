420d703d4f844b179da39f9bf74cf965b74ffbbd
MariD133 <60610461+MariD133@users.noreply.github.com>
2020-05-26 15:26:26 +0200

    Database_Access folder and db item, webconfig, .csproj file changes.

---
+++ YetAnotherNewsSite/wwwroot/SQLQuery Database Proc.sql
@@ -0,0 +1,13 @@
+﻿Create proc Sp_Articles_LazyLoad
+
+@PageIndex int,
+
+@PageSize int
+
+as
+
+begin
+
+Select * from Articles order by ArticleId asc offset @PageSize*(@PageIndex-1) Rows fetch next @PageSize rows only
+
+end
